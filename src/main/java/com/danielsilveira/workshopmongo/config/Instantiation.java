3ebccf32be5dcbbe2641ab1dd78d17fe9b31630e
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2025-08-06 19:03:11 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/danielsilveira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/danielsilveira/workshopmongo/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.danielsilveira.workshopmongo.config;
 
+import com.danielsilveira.workshopmongo.comment.CommentDTO;
 import com.danielsilveira.workshopmongo.post.AuthorDTO;
 import com.danielsilveira.workshopmongo.post.PostEntity;
 import com.danielsilveira.workshopmongo.post.PostRepository;
@@ -34,8 +35,16 @@ public class Instantiation implements CommandLineRunner {
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         PostEntity post1 = new PostEntity(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         PostEntity post2 = new PostEntity(null, sdf.parse("23/03/2018"), "Acordei feliz hoje!", "Tenha um ótimo dia!", new AuthorDTO(maria));
+
+        CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO comment2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(List.of(comment1, comment2));
+        post2.getComments().add(comment3);
 
         postRepository.saveAll(List.of(post1, post2));
 
--- src/main/java/com/danielsilveira/workshopmongo/post/PostEntity.java
+++ src/main/java/com/danielsilveira/workshopmongo/post/PostEntity.java
@@ -1,12 +1,15 @@
 package com.danielsilveira.workshopmongo.post;
 
+import com.danielsilveira.workshopmongo.comment.CommentDTO;
 import com.danielsilveira.workshopmongo.user.UserEntity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "post")
@@ -20,6 +23,8 @@ public class PostEntity implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public PostEntity() {}
 
@@ -70,6 +75,14 @@ public class PostEntity implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
